aa1afacc899a88d3a763b410bb506b4ef4dceffe
Dodik Gaghan <gaghan430@gmail.com>
2025-05-14 13:46:37 +0700

    fix: translation

--- app/modules/i18n/locales/en.ts
+++ app/modules/i18n/locales/en.ts
@@ -1,1 +1,1 @@
-/*eslint-disable*/import type{Messages}from"@lingui/core";export const messages=JSON.parse("{\"QcTGfQ\":[\"App Version:\"]}")as Messages;
+/*eslint-disable*/import type{Messages}from"@lingui/core";export const messages=JSON.parse("{\"QcTGfQ\":[\"App Version:\"],\"bdkkEh\":[\"Simplifying cloud operations\"],\"mL+CII\":[\"Welcome to Datum - Staff Portal\"]}")as Messages;
