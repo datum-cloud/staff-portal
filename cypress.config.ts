cacd91eabb040700d610f6691727deb03539132b
Dodik Gaghan <gaghan430@gmail.com>
2025-05-14 10:20:52 +0700

    feat: setup ci/cd

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,38 @@
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    setupNodeEvents(on, config) {},
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,
+    screenshotOnRunFailure: true,
+    experimentalStudio: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(message);
+
+          return null;
+        },
+      });
+
+      return config;
+    },
   },
   component: {
     devServer: {
       framework: 'react',
       bundler: 'vite',
     },
+    viewportWidth: 1280,
+    viewportHeight: 720,
     supportFile: 'cypress/support/component.tsx',
     specPattern: 'cypress/component/**/*.{cy,spec}.{js,jsx,ts,tsx}',
   },
