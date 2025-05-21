e50a0eabedef4f871ec7a1b954b44402092a75f1
Dodik Gaghan <gaghan430@gmail.com>
2025-05-21 08:50:27 +0700

    fix: duplicate entry server

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,13 +2,12 @@ import { loadCatalog } from '@/modules/i18n/lingui';
 import { i18n } from '@lingui/core';
 import { detect, fromHtmlTag } from '@lingui/detect-locale';
 import { I18nProvider } from '@lingui/react';
-import React, { StrictMode, startTransition } from 'react';
+import { StrictMode, startTransition } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
 
 async function main() {
   const locale = detect(fromHtmlTag('lang')) || 'en';
-
   await loadCatalog(locale);
 
   startTransition(() => {
