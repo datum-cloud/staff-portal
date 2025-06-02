9485cc9bb102a87d90dc7636a1f5ceb7f6e9a1d9
Dodik Gaghan <gaghan430@gmail.com>
2025-06-02 13:27:28 +0700

    [Fix #22] Add demo components

---
+++ app/components/demo/card-stats.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { Button } from '@/modules/shadcn/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/modules/shadcn/ui/card';
+import { ChartConfig, ChartContainer } from '@/modules/shadcn/ui/chart';
+import { Area, AreaChart, Line, LineChart, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    revenue: 10400,
+    subscription: 40,
+  },
+  {
+    revenue: 14405,
+    subscription: 90,
+  },
+  {
+    revenue: 9400,
+    subscription: 200,
+  },
+  {
+    revenue: 8200,
+    subscription: 278,
+  },
+  {
+    revenue: 7000,
+    subscription: 89,
+  },
+  {
+    revenue: 9600,
+    subscription: 239,
+  },
+  {
+    revenue: 11244,
+    subscription: 78,
+  },
+  {
+    revenue: 26475,
+    subscription: 89,
+  },
+];
+
+const chartConfig = {
+  revenue: {
+    label: 'Revenue',
+    color: 'var(--primary)',
+  },
+  subscription: {
+    label: 'Subscriptions',
+    color: 'var(--primary)',
+  },
+} satisfies ChartConfig;
+
+export function CardsStats() {
+  return (
+    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
+      <Card>
+        <CardHeader>
+          <CardDescription>Total Revenue</CardDescription>
+          <CardTitle className="text-3xl">$15,231.89</CardTitle>
+          <CardDescription>+20.1% from last month</CardDescription>
+        </CardHeader>
+        <CardContent className="pb-0">
+          <div style={{ width: '100%', height: '80px', minHeight: '80px' }}>
+            <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={80}>
+              <LineChart
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 10,
+                  left: 10,
+                  bottom: 0,
+                }}>
+                <Line
+                  type="monotone"
+                  strokeWidth={2}
+                  dataKey="revenue"
+                  stroke="var(--color-revenue)"
+                  activeDot={{
+                    r: 6,
+                  }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="pb-0 lg:hidden xl:flex">
+        <CardHeader>
+          <CardDescription>Subscriptions</CardDescription>
+          <CardTitle className="text-3xl">+2,350</CardTitle>
+          <CardDescription>+180.1% from last month</CardDescription>
+          <CardAction>
+            <Button variant="ghost" size="sm">
+              View More
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent className="mt-auto max-h-[124px] flex-1 p-0">
+          <div style={{ width: '100%', height: '124px', minHeight: '124px' }}>
+            <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={124}>
+              <AreaChart
+                data={data}
+                margin={{
+                  left: 0,
+                  right: 0,
+                }}>
+                <Area
+                  dataKey="subscription"
+                  fill="var(--color-subscription)"
+                  fillOpacity={0.05}
+                  stroke="var(--color-subscription)"
+                  strokeWidth={2}
+                  type="monotone"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,4 +1,16 @@
 import type { Route } from './+types/dashboard';
+import { CardsCreateAccount } from '@/components/demo/card-account';
+import { CardsActivityGoal } from '@/components/demo/card-activity';
+import { CardsCalendar } from '@/components/demo/card-calendar';
+import { CardsChat } from '@/components/demo/card-chat';
+import { CardsCookieSettings } from '@/components/demo/card-cookie';
+import { CardsExerciseMinutes } from '@/components/demo/card-exercise';
+import { CardsForms } from '@/components/demo/card-form';
+import { CardsPayments } from '@/components/demo/card-payment';
+import { CardsReportIssue } from '@/components/demo/card-report';
+import { CardsShare } from '@/components/demo/card-share';
+import { CardsStats } from '@/components/demo/card-stats';
+import { CardsTeamMembers } from '@/components/demo/card-team';
 import { metaObject } from '@/utils/helpers';
 
 export const meta: Route.MetaFunction = () => {
@@ -7,10 +19,51 @@ export const meta: Route.MetaFunction = () => {
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      {Array.from({ length: 24 }).map((_, index) => (
-        <div key={index} className="bg-muted/50 aspect-video h-12 w-full rounded-lg" />
-      ))}
+    <div className="md:grids-col-2 grid p-4 **:data-[slot=card]:shadow-none md:gap-4 lg:grid-cols-10 xl:grid-cols-11">
+      <div className="grid gap-4 lg:col-span-4 xl:col-span-6">
+        <CardsStats />
+        <div className="grid gap-1 sm:grid-cols-[auto_1fr] md:hidden">
+          <CardsCalendar />
+          <div className="pt-3 sm:pt-0 sm:pl-2 xl:pl-4">
+            <CardsActivityGoal />
+          </div>
+          <div className="pt-3 sm:col-span-2 xl:pt-4">
+            <CardsExerciseMinutes />
+          </div>
+        </div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
+          <div className="flex flex-col gap-4">
+            <CardsForms />
+            <CardsTeamMembers />
+            <CardsCookieSettings />
+          </div>
+          <div className="flex flex-col gap-4">
+            <CardsCreateAccount />
+            <CardsChat />
+            <div className="hidden xl:block">
+              <CardsReportIssue />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 lg:col-span-6 xl:col-span-5">
+        <div className="hidden gap-1 sm:grid-cols-[auto_1fr] md:grid">
+          <CardsCalendar />
+          <div className="pt-3 sm:pt-0 sm:pl-2 xl:pl-3">
+            <CardsActivityGoal />
+          </div>
+          <div className="pt-3 sm:col-span-2 xl:pt-3">
+            <CardsExerciseMinutes />
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <CardsPayments />
+        </div>
+        <CardsShare />
+        <div className="xl:hidden">
+          <CardsReportIssue />
+        </div>
+      </div>
     </div>
   );
 }
