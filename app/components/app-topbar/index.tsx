0ad4580d9663a1fee18ce08239046a6cd73b28de
Dodik Gaghan <gaghan430@gmail.com>
2025-07-14 15:56:32 +0700

    [Fix #79] Remove search

--- app/components/app-topbar/index.tsx
+++ app/components/app-topbar/index.tsx
@@ -1,4 +1,3 @@
-import AppSearch from '@/components/app-search';
 import ThemeSwitcher from '@/components/theme-switcher';
 import { Separator } from '@/modules/shadcn/ui/separator';
 import { SidebarTrigger } from '@/modules/shadcn/ui/sidebar';
@@ -8,7 +7,7 @@ const AppTopbar = () => {
     <header className="bg-background sticky top-0 z-10 flex h-12 shrink-0 items-center gap-2 border-b px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <SidebarTrigger className="-ml-1" />
       <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
-      <AppSearch />
+      {/* <AppSearch /> */}
       <div className="ml-auto flex items-center space-x-4">
         <ThemeSwitcher />
       </div>
