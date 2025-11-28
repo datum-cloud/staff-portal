774de232bf8d11a74ddd1420c642898978ab5ec4
Dodik Gaghan <gaghan430@gmail.com>
2025-11-28 17:23:51 +0700

    Fixing unit tests

--- app/resources/request/client/project.request.test.ts
+++ app/resources/request/client/project.request.test.ts
@@ -16,6 +16,8 @@ describe('project.request', () => {
   let projectDomainStatusQuery: typeof import('./project.request').projectDomainStatusQuery;
   let projectDeleteMutation: typeof import('./project.request').projectDeleteMutation;
   let projectDnsListQuery: typeof import('./project.request').projectDnsListQuery;
+  let projectDnsRecordListQuery: typeof import('./project.request').projectDnsRecordListQuery;
+  let projectDnsRecordStatusQuery: typeof import('./project.request').projectDnsRecordStatusQuery;
 
   beforeEach(async () => {
     vi.clearAllMocks();
@@ -29,6 +31,8 @@ describe('project.request', () => {
     projectDomainStatusQuery = mod.projectDomainStatusQuery;
     projectDeleteMutation = mod.projectDeleteMutation;
     projectDnsListQuery = mod.projectDnsListQuery;
+    projectDnsRecordListQuery = mod.projectDnsRecordListQuery;
+    projectDnsRecordStatusQuery = mod.projectDnsRecordStatusQuery;
   }, 20000);
 
   test('projectListQuery with search', async () => {
@@ -55,7 +59,7 @@ describe('project.request', () => {
     await projectExportPolicyListQuery('proj', { limit: 3 });
     expect(axiosMock.apiRequestClient).toHaveBeenCalledWith({
       method: 'GET',
-      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/telemetry.miloapis.com/v1alpha1/namespaces/default/exportpolicies',
+      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/telemetry.miloapis.com/v1alpha1/exportpolicies',
       params: { limit: 3 },
     });
   });
@@ -64,8 +68,25 @@ describe('project.request', () => {
     await projectDnsListQuery('proj', { limit: 4, cursor: 'n3' });
     expect(axiosMock.apiRequestClient).toHaveBeenCalledWith({
       method: 'GET',
-      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/networking.datumapis.com/v1alpha/dnszones',
+      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/dns.networking.miloapis.com/v1alpha1/dnszones',
       params: { limit: 4, continue: 'n3' },
+    });
+  });
+
+  test('projectDnsRecordListQuery', async () => {
+    await projectDnsRecordListQuery('proj', 'example.com', 'default');
+    expect(axiosMock.apiRequestClient).toHaveBeenCalledWith({
+      method: 'GET',
+      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/dns.networking.miloapis.com/v1alpha1/namespaces/default/dnsrecordsets',
+      params: { fieldSelector: 'spec.dnsZoneRef.name=example.com' },
+    });
+  });
+
+  test('projectDnsRecordStatusQuery', async () => {
+    await projectDnsRecordStatusQuery('proj', 'example.com', 'default');
+    expect(axiosMock.apiRequestClient).toHaveBeenCalledWith({
+      method: 'GET',
+      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/dns.networking.miloapis.com/v1alpha1/namespaces/default/dnsrecordsets/example.com/status',
     });
   });
 
@@ -82,7 +103,7 @@ describe('project.request', () => {
     await projectDomainStatusQuery('proj', 'example.com');
     expect(axiosMock.apiRequestClient).toHaveBeenCalledWith({
       method: 'GET',
-      url: 'apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/networking.datumapis.com/v1alpha/namespaces/default/domains/example.com/status',
+      url: '/apis/resourcemanager.miloapis.com/v1alpha1/projects/proj/control-plane/apis/networking.datumapis.com/v1alpha/namespaces/default/domains/example.com/status',
     });
   });
 
